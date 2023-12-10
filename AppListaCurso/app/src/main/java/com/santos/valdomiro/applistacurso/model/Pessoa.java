748ca90bd928cc3eeec0447224d8f9dd6a9c7292
Valdomiro Santos <valdomiro.jose@outlook.com>
2023-12-10 09:04:56 -0300

    "Adicionando classe Pessoa()"

---
+++ AppListaCurso/app/src/main/java/com/santos/valdomiro/applistacurso/model/Pessoa.java
@@ -0,0 +1,43 @@
+package com.santos.valdomiro.applistacurso.model;
+
+public class Pessoa {
+
+    private String primeiroNome;
+    private String sobrenome;
+    private String cursoDesejado;
+    private String telefoneContato;
+
+    public Pessoa() {}
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
+}
--- AppListaCurso/app/src/main/java/com/santos/valdomiro/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/com/santos/valdomiro/applistacurso/view/MainActivity.java
@@ -1,14 +1,22 @@
-package com.santos.valdomiro.applistacurso;
+package com.santos.valdomiro.applistacurso.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.santos.valdomiro.applistacurso.R;
+import com.santos.valdomiro.applistacurso.model.Pessoa;
+
 public class MainActivity extends AppCompatActivity {
+
+    Pessoa pessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pessoa = new Pessoa();
+
     }
 }
